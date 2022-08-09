f9121035332626444d00018b1a2d506432f0358c
NguyenNhatTruongCTU <103418972+NguyenNhatTruongCTU@users.noreply.github.com>
2022-08-09 18:05:33 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,21 +1,21 @@
 var student = {
     "student1": {
-        "hoten": "Harry",
+        "hoten": "Harry Potter",
         "mssv": "B10000",
-        "birth": "01/01/2357",
+        "birth": "01/01/10001",
         "nganh": "Care of Magical Creatures",
         "malop": "First",
         "email": "@gmail.com",
         "image": "images/khoa.jpg",
-        "address": "Diagon Alley",
+        "address": "Middle Earth",
         "phone": "9 3/4",
         "noidung": "...",
         "donggop": 100
     },
     "student2": {
-        "hoten": "Potter",
+        "hoten": "Tom Riddle",
         "mssv": "B10000",
-        "birth": "01/01/2357",
+        "birth": "01/01/10001",
         "nganh": "Defence Against the Dark Arts, Astronomy and Herbology",
         "malop": "First",
         "email": "@gmail.com",
@@ -26,22 +26,22 @@ var student = {
         "donggop": 100
     },
     "student3": {
-        "hoten": "Hermione",
+        "hoten": "Paul Atreides",
         "mssv": "B10000",
-        "birth": "01/01/2357",
+        "birth": "01/01/10001",
         "nganh": "Charms",
         "malop": "First",
         "email": "@gmail.com",
         "image": "images/khoa.jpg",
-        "address": "The Leaky Cauldron",
+        "address": "Westeros",
         "phone": "9 3/4",
         "noidung": "...",
         "donggop": 100
     },
     "student4": {
-        "hoten": "Ron",
+        "hoten": "Aegon Targaryen",
         "mssv": "B10000",
-        "birth": "01/01/2357",
+        "birth": "01/01/10001",
         "nganh": "Transfiguration",
         "malop": "First",
         "email": "@gmail.com",
@@ -52,9 +52,9 @@ var student = {
         "donggop": 100
     },
     "student5": {
-        "hoten": "Sirius",
+        "hoten": "Anakin Skywalker",
         "mssv": "B10000",
-        "birth": "01/01/2357",
+        "birth": "01/01/10001",
         "nganh": "History of Magic",
         "malop": "First",
         "email": "@gmail.com",
